0d4ff2649f416d7bed904379ce9826eb769d930b
mahnoosh92 <mahnooshmahdavi2012@gmail.com>
2023-01-08 20:13:57 +0000

    Dependency added

--- app/src/main/java/com/mahdavi/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/mahdavi/weatherapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mahdavi.weatherapp
+package com.mahdavi.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mahdavi.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

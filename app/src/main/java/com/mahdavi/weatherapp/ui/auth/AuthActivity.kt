49f3e7e890b421b536306fd9cbf3d8d82781728a
mahnoosh92 <mahnooshmahdavi2012@gmail.com>
2023-01-09 19:53:26 +0000

    navigation is being implemented

---
+++ app/src/main/java/com/mahdavi/weatherapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.mahdavi.weatherapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahdavi.weatherapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/mahdavi/weatherapp/ui/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,26 @@
+package com.mahdavi.weatherapp.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahdavi.weatherapp.R
+import com.mahdavi.weatherapp.databinding.ActivityOnBoardingBinding
+import com.mahdavi.weatherapp.ui.onboarding.splash.SplashFragment
+import com.mahdavi.weatherapp.utils.extensions.addFragment
+import com.mahdavi.weatherapp.utils.extensions.replaceFragment
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addFragment(OnBoardingFragment(), binding.container.id, false)
+    }
+
+    fun navigateToSplash() {
+        replaceFragment(SplashFragment(), binding.container.id, true)
+    }
+}
